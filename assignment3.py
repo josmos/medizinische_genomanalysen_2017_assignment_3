756e12b893d0b02c1d27e32bf3c8a558690d0dbd
josmos <josmos43@gmail.com>
2017-03-22 16:03:27 +0100

    simultaneous iteration debugged

--- assignment3.py
+++ assignment3.py
@@ -1,67 +1,115 @@
 #! /usr/bin/env python2
 
-import vcf
-import hgvs
+import vcf.utils
+from cyvcf2 import VCF, Writer
+import hgvs.parser
+import os
+import subprocess
 
-__author__ = 'XXX'
+__author__ = 'Josef Moser'
 
 
 class Assignment3:
-    
     def __init__(self):
-        ## Check if pyvcf is installed
         print("PyVCF version: %s" % vcf.VERSION)
-        ## Check if hgvs is installed
         print("HGVS version: %s" % hgvs.__version__)
-        
+        self.son = self.get_file("ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/"
+                                 "analysis/IonTorrent_TVC_03162015/AmpliseqExome.20141120.NA24385.vcf")
+        self.mother = self.get_file("ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/"
+                                    "analysis/IonTorrent_TVC_03162015/AmpliseqExome.20141120.NA24143.vcf")
+        self.father = self.get_file("ftp://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/"
+                                    "analysis/IonTorrent_TVC_03162015/AmpliseqExome.20141120.NA24149.vcf")
+
+        # check if vcftools is installed:
+        try:
+            with open(os.devnull, 'w') as fnull:
+                subprocess.call("vcf-merge -h",  stdout=fnull, stderr=subprocess.PIPE, shell=True)
+        except subprocess.CalledProcessError as grepexc:
+            print "error code", grepexc.returncode, grepexc.output
+
+    @staticmethod
+    def get_file(ftp_link):
+        bn = os.path.basename(ftp_link)
+        if not os.path.isfile(bn):
+            subprocess.call(["wget", ftp_link])
+            subprocess.call(" ".join(["bgzip", "-c", bn, ">", bn + ".gz"]), shell=True)
+            subprocess.call(["tabix", "-p", "vcf", bn + ".gz"])
+
+        return bn
 
     def get_total_number_of_variants_mother(self):
         '''
         Return the total number of identified variants in the mother
         :return: 
         '''
-        print("TODO")
-        
-        
+        print "Variants in mother: %d" % len(list(VCF(self.mother)))
+
     def get_total_number_of_variants_father(self):
         '''
         Return the total number of identified variants in the father
         :return: 
         '''
-        print("TODO")
-       
-        
+        print "Variants in father: %d" % len(list(VCF(self.father)))
+
+    @staticmethod
+    def compare_parent_child(parent, child):
+        both = []
+        for p, c in vcf.utils.walk_together(parent, child):
+                if p and c \
+                        and p.CHROM == c.CHROM \
+                        and p.ALT == c.ALT \
+                        and p.REF == c.REF \
+                        and p.POS == c.POS \
+                        and p.INFO.get("GT") == c.INFO.get("GT"):
+                    both.append(c)
+
+        return both
+
     def get_variants_shared_by_father_and_son(self):
         '''
         Return the number of identified variants shared by father and son
-        :return: 
+        :return:
         '''
-        print("TODO")
-        
-        
+        self.father_and_son = self.compare_parent_child(VCF(self.father), VCF(self.son))
+        print "Variants shared by father and son: %d" % len(self.father_and_son)
+
     def get_variants_shared_by_mother_and_son(self):
         '''
         Return the number of identified variants shared by mother and son
         :return: 
         '''
-        print("TODO")
-        
+        self.mother_and_son = self.compare_parent_child(VCF(self.mother), VCF(self.son))
+        print "Variants shared by mother and son: %d" % len(self.mother_and_son)
+
     def get_variants_shared_by_trio(self):
         '''
         Return the number of identified variants shared by father, mother and son
         :return: 
         '''
-        print("TODO")
-        
+        m = VCF(self.mother)
+        f = VCF(self.father)
+        s = VCF(self.son)
+        all = []
+        for x in vcf.utils.walk_together(m, f, s):
+            m, f, s = x[0], x[1], x[2]
+            if m and f and s \
+                    and m.CHROM == f.CHROM == s.CHROM \
+                    and m.ALT == f.ALT == s.ALT \
+                    and m.REF == f.REF == s.REF \
+                    and m.POS == f.POS == s.POS \
+                    and m.INFO.get("GT") == f.INFO.get("GT") == s.INFO.get("GT"):
+                all.append(m)
+
+        print "Variants shared by father, mother and son: %d" % len(all)
 
     def merge_mother_father_son_into_one_vcf(self):
         '''
         Creates one VCF containing all variants of the trio (merge VCFs)
         :return: 
         '''
-        print("TODO")
-        
-        
+
+        subprocess.call(" ".join(["vcf-merge", self.father, self.mother, self.son, ">", "test.vcf"]), shell=True)
+
     def convert_first_variants_of_son_into_HGVS(self):
         '''
         Convert the first 100 variants identified in the son into the corresponding transcript HGVS.
@@ -69,11 +117,16 @@ class Assignment3:
         - https://hgvs.readthedocs.io/en/master/examples/manuscript-example.html#project-genomic-variant-to-a-new-transcript
         :return: 
         '''
-        print("TODO")
-        
+        hgvs_parser = hgvs.parser.Parser()
     
     def print_summary(self):
-        print("Print all results here")
+        print(__author__)
+        self.get_total_number_of_variants_mother()
+        self.get_total_number_of_variants_father()
+        self.get_variants_shared_by_father_and_son()
+        self.get_variants_shared_by_mother_and_son()
+        self.get_variants_shared_by_trio()
+        #self.merge_mother_father_son_into_one_vcf()
     
         
 if __name__ == '__main__':
---
+++ install_vcftools.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+
+subprocess.call(["wget", "https://github.com/vcftools/vcftools/archive/master.zip"])
+subprocess.call(["unzip", "master.zip"])
+path = os.path.join(os.getcwd(), "vcftools-master")
+perlpath = os.path.join(path, "src/perl/")
+subprocess.call(["export", "PERL5LIB=" + perlpath], shell=True)
+subprocess.check_call("./autogen.sh", shell=True, cwd=path)
+subprocess.call("./configure", shell=True, cwd=path)
+subprocess.call("make", shell=True, cwd=path)
+subprocess.call("make install", shell=True, cwd=path) # needs sudo
